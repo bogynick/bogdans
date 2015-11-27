6aefa94d36ce3c0cc47fa215f72974c294bd5e1b
Piotr Nowojski <piotr.nowojski@teradata.com>
2015-11-27 09:15:44 +0100

    Refactor ROW to generalized Parameters

--- presto-spi/src/main/java/com/facebook/presto/spi/type/TypeManager.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/type/TypeManager.java
@@ -32,7 +32,7 @@ public interface TypeManager
      * Gets the type with the specified base type, and the given parameters, or null if not found.
      */
     @Deprecated
-    Type getParameterizedType(String baseTypeName, List<TypeSignature> typeParameters, List<Object> literalParameters);
+    Type getParameterizedType(String baseTypeName, List<TypeSignature> typeParameters, List<String> literalParameters);
 
     /**
      * Gets a list of all registered types.
