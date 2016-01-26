prestodb/presto

Add bindLiteralParameters method and 'parametersTypes' argument to specialize(...) function
Adding bindLiteralParameters(...) method and 'parameterTypes' argument to specialize(...) function
allows to bind literal parameters during function specialization. This allows to compute derivate
variables that are partially applied to specialized function implementation.

presto-main/src/main/java/com/facebook/presto/metadata/Signature.java
@@ -41,6 +41,7 @@ import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.any;
 
 public final class Signature
 {
@@ -164,20 +165,28 @@ public final class Signature
 
     public Signature resolveCalculatedTypes(List<TypeSignature> parameterTypes)
     {
-        if (!returnType.isCalculated()) {
+        if (isReturnTypeOrAnyArgumentTypeCalculated()) {
             return this;
         }
 
-        Map<String, OptionalLong> inputs = new HashMap<>();
+        Map<String, OptionalLong> inputs = bindLiteralParameters(parameterTypes);
+        TypeSignature calculatedReturnType = TypeUtils.resolveCalculatedType(returnType, inputs);
+        return new Signature(name, calculatedReturnType, parameterTypes);
+    }
+
+    public Map<String, OptionalLong> bindLiteralParameters(List<TypeSignature> parameterTypes)
+    {
+        Map<String, OptionalLong> boundParameters = new HashMap<>();
+
         for (int index = 0; index < argumentTypes.size(); index++) {
             TypeSignature argument = argumentTypes.get(index);
             if (argument.isCalculated()) {
                 TypeSignature actualParameter = parameterTypes.get(index);
-                inputs.putAll(TypeUtils.extractCalculationInputs(argument, actualParameter));
+                boundParameters.putAll(TypeUtils.extractCalculationInputs(argument, actualParameter));
             }
         }
-        TypeSignature calculatedReturnType = TypeUtils.resolveCalculatedType(returnType, inputs);
-        return new Signature(name, calculatedReturnType, parameterTypes);
+
+        return boundParameters;
     }
 
     @Override
@@ -259,6 +268,11 @@ public final class Signature
         checkState(boundParameters.keySet().equals(parameters.keySet()), "%s matched arguments %s, but type parameters %s are still unbound", this, types, Sets.difference(parameters.keySet(), boundParameters.keySet()));
 
         return boundParameters;
+    }
+
+    private boolean isReturnTypeOrAnyArgumentTypeCalculated()
+    {
+        return !returnType.isCalculated() && !any(argumentTypes, TypeSignature::isCalculated);
     }
 
     private static boolean matchArguments(

bogynick/bogdans

presto-main/src/main/java/com/facebook/presto/metadata/Signature.java
@@ -43,6 +43,7 @@ import static com.facebook.presto.type.TypeRegistry.canCoerce;
 import static com.facebook.presto.util.ImmutableCollectors.toImmutableList;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
+import static com.google.common.collect.Iterables.any;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
@@ -130,7 +131,7 @@ public final class Signature
         this(name, kind, returnType, ImmutableList.copyOf(argumentTypes));
     }
 
-    public static Signature internalOperator(OperatorType operator, Type returnType, List<? extends  Type> argumentTypes)
+    public static Signature internalOperator(OperatorType operator, Type returnType, List<? extends Type> argumentTypes)
     {
         return internalScalarFunction(mangleOperatorName(operator.name()), returnType.getTypeSignature(), argumentTypes.stream().map(Type::getTypeSignature).collect(toImmutableList()));
     }
@@ -203,24 +204,30 @@ public final class Signature
 
     public Signature resolveCalculatedTypes(List<TypeSignature> parameterTypes)
     {
-        if (!returnType.isCalculated() && argumentTypes.stream().noneMatch(TypeSignature::isCalculated)) {
+        if (isReturnTypeOrAnyArgumentTypeCalculated()) {
             return this;
         }
 
-        Map<String, OptionalLong> inputs = new HashMap<>();
-        for (int index = 0; index < argumentTypes.size(); index++) {
-            TypeSignature argument = argumentTypes.get(index);
-            if (argument.isCalculated()) {
-                TypeSignature actualParameter = parameterTypes.get(index);
-                inputs.putAll(TypeUtils.computeParameterBindings(argument, actualParameter));
-            }
-        }
+        Map<String, OptionalLong> inputs = bindLiteralParameters(parameterTypes);
         TypeSignature calculatedReturnType = TypeUtils.resolveCalculatedType(returnType, inputs, true);
         return new Signature(
                 name,
                 kind,
                 calculatedReturnType,
                 argumentTypes.stream().map(parameter -> TypeUtils.resolveCalculatedType(parameter, inputs, false)).collect(toImmutableList()));
+    }
+
+    public Map<String, OptionalLong> bindLiteralParameters(List<TypeSignature> parameterTypes)
+    {
+        Map<String, OptionalLong> boundParameters = new HashMap<>();
+        for (int index = 0; index < argumentTypes.size(); index++) {
+            TypeSignature argument = argumentTypes.get(index);
+            if (argument.isCalculated()) {
+                TypeSignature actualParameter = parameterTypes.get(index);
+                boundParameters.putAll(TypeUtils.computeParameterBindings(argument, actualParameter));
+            }
+        }
+        return boundParameters;
     }
 
     @Override
@@ -302,6 +309,11 @@ public final class Signature
         checkState(boundParameters.keySet().equals(parameters.keySet()), "%s matched arguments %s, but type parameters %s are still unbound", this, types, Sets.difference(parameters.keySet(), boundParameters.keySet()));
 
         return boundParameters;
+    }
+
+    private boolean isReturnTypeOrAnyArgumentTypeCalculated()
+    {
+        return !returnType.isCalculated() && !any(argumentTypes, TypeSignature::isCalculated);
     }
 
     private static boolean matchArguments(